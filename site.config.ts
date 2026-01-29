15aa6af34c3ceb47b321fee590906420f505f906
slavik <you@example.com>
2026-01-29 19:54:25 +0300

    1488

---
+++ site.config.ts
@@ -0,0 +1,22 @@
+export const siteConfig = {
+  title: "DragonDEX Hunt",
+  description: "DragonDEX Hunt",
+  organisation: "DragonDEX",
+  tokenName: "DragonDEX",
+  tokenShortName: "DragonDEX",
+  // Если лендинг позиционируется как отдельный сайт для airdrop'а
+  mainSite: "",
+  twitterUrl: "https://x.com/dragonswap_dex",
+  telegramUrl: "https://t.me/dragonswaphq",
+  redditUrl: "/",
+  coinmarketcupUrl: "https://coinmarketcap.com/currencies/dragonswap/",
+  theme: "dark",
+  color: "#9FD919",
+  faviconIcoUrl: "https://i-p.rmcdn.net/64f4527e45849b003dc9348a/5167502/image-6fb68a45-3e59-4970-bd09-904e205d0171.png?w=115&e=webp&nll=true&cX=109&cY=0&cW=2709&cH=2817",
+  // logoUrl: "https://app.ethena.fi/custom/logo.svg",
+  // mainImageUrl: "https://app.ethena.fi/shared/ethena.svg",
+  mainImageUrl:
+    "./token-3d.png",
+  mintSumUsd: 1100000,
+  timeSec: 60 * 60,
+};
